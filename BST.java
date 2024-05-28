1ad5059a856d5939a046893fef1b8fd53535c7c3
rohitsmart <rohitsmart95@gmail.com>
2024-05-28 11:19:58 +0530

    update

--- BST.java
+++ BST.java
@@ -55,7 +55,95 @@ public class BST {
             inOrder(root.right);
         }
     }
-   
+
+    void preOrderTreeTraversal()
+    {
+        preOrder(root);
+    }
+
+    void preOrder(Node root)
+    {
+        if(root!=null)
+        {
+            System.out.print(" "+root.key);
+            preOrder(root.left);
+            preOrder(root.right);
+        }
+
+    }
+
+    void postOrderTreeTraversal()
+    {
+        postOrder(root);
+    }
+
+    void postOrder(Node root)
+    {
+        if(root!=null)
+        {
+            postOrder(root.left);
+            postOrder(root.right);
+            System.out.print(" "+root.key);
+
+        }
+
+    }
+
+    int search(int key) {
+        return searchRecord(root, key, 0);
+    }
+
+    int searchRecord(Node root, int key, int depth) {
+        if (root == null) {
+            return -1; // Key not found
+        }
+
+        if (root.key == key) {
+            return depth;
+        }
+
+        if (key < root.key) {
+            return searchRecord(root.left, key, depth + 1);
+        } else {
+            return searchRecord(root.right, key, depth + 1);
+        }
+    }
+
+    void deleteKey(int key) {
+        root = deleteRec(root, key);
+    }
+
+    
+    Node deleteRec(Node root, int key) {
+        if (root == null)
+            return root;
+
+        if (key < root.key)
+            root.left = deleteRec(root.left, key);
+        else if (key > root.key)
+            root.right = deleteRec(root.right, key);
+        else {
+            if (root.left == null)
+                return root.right;
+            else if (root.right == null)
+                return root.left;
+
+            root.key = minValue(root.right);
+            root.right = deleteRec(root.right, root.key);
+        }
+        return root;
+    }
+
+    int minValue(Node root) {
+        int minv = root.key;
+        while (root.left != null) {
+            minv = root.left.key;
+            root = root.left;
+        }
+        return minv;
+    }
+
+
     public static void main(String[] args) {
         System.out.println("creating tree::");
         BST tree = new BST();
@@ -70,6 +158,25 @@ public class BST {
         tree.insert(60);
         tree.insert(80);
 
+        System.out.println("In Order tree traversal");
+
         tree.inOrderTreeTraversal();
-    }
+
+        System.out.println("\nPre Order tree traversal");
+
+        tree.preOrderTreeTraversal();
+
+        System.out.println("\nPost Order tree traversal");
+
+        tree.postOrderTreeTraversal();
+
+        System.out.println(" \nsearching 50 :"+tree.search(90));
+
+        System.out.println("\n\nDelete 70");
+        tree.deleteKey(70);   
+
+        System.out.println("\nIn Order tree traversal");
+
+        tree.inOrderTreeTraversal();
+     }
 }
---
+++ practise.js
@@ -0,0 +1,8 @@
+/*creating object*/
+
+var object={
+    name:"rohit",
+    age:"20"
+}
+
+console.log
